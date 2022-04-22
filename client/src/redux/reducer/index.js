fc7cd087bfa8d0d01836efc44a767bf54cf7d0a6
Martín Violi <martinvioli29@gmail.com>
2022-04-22 20:00:54 -0300

    repository upload

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,6 @@
+// import {actions vars here} from "../actions";
+
+// STATE CREATION
+const initialState = {};
+
+// export default function reducerName(state = initialState, action){here goes the switch cases or if's... || Remember that the default always is return initialState}
---
+++ client/src/redux/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+// import reducerName from "../reducer";
+
+// Thunk para las operaciones asyncronas en relacion al store.
+import thunk from "redux-thunk";
+
+// const store = createStore(reducerName, applyMiddleware(thunk));
+
+// export default store;
