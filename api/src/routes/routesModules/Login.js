a176477f8b3eafd364a44f46e9a3ea7c30c53e2c
Lean97-start <Leandro_mza008@hotmail.com>
2022-04-27 15:56:38 -0300

    Fix login params

--- api/src/routes/routesModules/Login.js
+++ api/src/routes/routesModules/Login.js
@@ -5,7 +5,7 @@ const router = Router();
 const user = new User();
 
 router.post('/', async (req, res) => {
-    const {email, userName, password} = req.params;
+    const {email, userName, password} = req.body;
     const responseLoginUser = await user.loginUser(email, userName, password);
     //Validar si tiene una propiedad msg, de ser asi regresamos status(404) y el msg 
     res.send(responseLoginUser);
