6e425787fa383daef9b23231958721a4c7b4d578
unknown <facumazzei@hotmail.com>
2022-04-30 15:11:00 -0300

    Unified Search route in index.js

--- api/src/routes/AlbumByName.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-const Search = require('../Controllers/Search')
-
-const search = new Search()
-
-router.get('/album', search.searchAlbumByName)
-
-module.exports = router;
