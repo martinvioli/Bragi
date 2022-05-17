c1cfbd07bbe8d6bea23fd3d4b5bc6221a4726719
Lean97-start <Leandro_mza008@hotmail.com>
2022-05-17 16:29:55 -0300

    Verify Admin pass

--- api/src/routes/Admin.js
+++ api/src/routes/Admin.js
@@ -1,47 +1,45 @@
 const { Router } = require("express");
 const Admin = require("../Controllers/Admin");
-const { verifyToken , verifyAdmin} = require("../middlewares/authjwt");
+const {verifyAdmin} = require("../middlewares/authjwt");
 
 const router = Router();
 const admin = new Admin();
 
-router.post('/verify', verifyAdmin);
-
 //Estadisticas
-router.get("/getUserStandar", admin.getUserStandar);
-router.get("/getUserPremium", admin.getUserPremium);
-router.get("/getUserArtist", admin.getUserArtist);
-router.get("/getActiveMembership", admin.getPremiumActiveAccounts);
-router.get("/getInactiveMembership", admin.getPremiumInactiveAccounts);
-router.get("/getDebtorMembership", admin.getPremiumDebtorAccounts);
-router.get("/getAllPostUsers", admin.getAllPosts);
+router.get("/getUserStandar",[verifyAdmin],admin.getUserStandar);
+router.get("/getUserPremium",[verifyAdmin], admin.getUserPremium);
+router.get("/getUserArtist",[verifyAdmin], admin.getUserArtist);
+router.get("/getActiveMembership",[verifyAdmin], admin.getPremiumActiveAccounts);
+router.get("/getInactiveMembership",[verifyAdmin], admin.getPremiumInactiveAccounts);
+router.get("/getDebtorMembership",[verifyAdmin], admin.getPremiumDebtorAccounts);
+router.get("/getAllPostUsers",[verifyAdmin], admin.getAllPosts);
 
 //Posteos Admin
-router.get("/allPostAdmin", admin.getPostsAdmin);
-router.post("/createPostAdmin", admin.createPostAdmin);
-router.post("/editPostAdmin", admin.updatePostAdmin);
-router.post("/deletePostAdmin", admin.deletePostAdmin);
+router.get("/allPostAdmin",[verifyAdmin], admin.getPostsAdmin);
+router.post("/createPostAdmin",[verifyAdmin], admin.createPostAdmin);
+router.post("/editPostAdmin",[verifyAdmin], admin.updatePostAdmin);
+router.post("/deletePostAdmin",[verifyAdmin], admin.deletePostAdmin);
 
 //Reports
-router.get("/reports/reportsUser", admin.getUserReport);
-router.get("/reports/reportsPost", admin.getPostReport);
-router.get("/reports/reportsComment", admin.getCommentReport);
-router.get("/reports/:idReport", admin.getReport);
-router.post("/reports/deletePost", admin.deletePostReport);
-router.post("/reports/allowPost", admin.allowPostReport);
-router.post("/reports/deleteComment", admin.deleteCommentReport);
-router.post("/reports/allowComment", admin.allowCommentReport);
-router.get("/reports", admin.allReport);
+router.get("/reports/reportsUser",[verifyAdmin], admin.getUserReport);
+router.get("/reports/reportsPost",[verifyAdmin], admin.getPostReport);
+router.get("/reports/reportsComment",[verifyAdmin], admin.getCommentReport);
+router.get("/reports/:idReport",[verifyAdmin], admin.getReport);
+router.post("/reports/deletePost",[verifyAdmin], admin.deletePostReport);
+router.post("/reports/allowPost",[verifyAdmin], admin.allowPostReport);
+router.post("/reports/deleteComment",[verifyAdmin], admin.deleteCommentReport);
+router.post("/reports/allowComment",[verifyAdmin], admin.allowCommentReport);
+router.get("/reports",[verifyAdmin], admin.allReport);
 
 //User Management
-router.get("/allBannedUser", admin.getAllBannedUser);
-router.post("/banUser", admin.banUser);
-router.post("/unbanUser", admin.unbanUser);
+router.get("/allBannedUser",[verifyAdmin], admin.getAllBannedUser);
+router.post("/banUser",[verifyAdmin], admin.banUser);
+router.post("/unbanUser",[verifyAdmin], admin.unbanUser);
 
 //Plan Premium
-router.post("/premiumPlan/create", admin.creatPremiumPlan);
-router.post("/premiumPlan/edition", admin.editPremiumPlan);
-router.post("/premiumPlan/delete", admin.cancelPremiumPlan);
-router.get("/premiumPlan/premiumPlans", admin.getPremiumPlan);
+router.post("/premiumPlan/create",[verifyAdmin], admin.creatPremiumPlan);
+router.post("/premiumPlan/edition",[verifyAdmin], admin.editPremiumPlan);
+router.post("/premiumPlan/delete",[verifyAdmin], admin.cancelPremiumPlan);
+router.get("/premiumPlan/premiumPlans",[verifyAdmin], admin.getPremiumPlan);
 
 module.exports = router;
