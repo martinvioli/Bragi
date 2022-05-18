b55c6539971a40324e8c0ebafbf6972a7d26fde6
MartinVioli <martinvioli29@gmail.com>
2022-05-18 16:35:34 -0300

    ultimatum fixes

--- api/src/routes/Pay.js
+++ api/src/routes/Pay.js
@@ -1,12 +1,11 @@
-const {Router} = require('express');
-const Pay = require('../Controllers/Pay');
+const { Router } = require("express");
+const Pay = require("../Controllers/Pay");
 
 const router = Router();
 const pay = new Pay();
 
-router.post('/', pay.payment);
-router.post('/sub', pay.sub)
-router.post('/convertToPremium', pay.convertToPremium)
-
+router.post("/", pay.payment);
+router.post("/sub", pay.sub);
+/* router.post('/convertToPremium', pay.convertToPremium) */
 
 module.exports = router;
